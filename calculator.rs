c2bc5d79e2b7bcb5c62ad63d36344c1bba1bfa43
Lukas2026 <62200410+Lukas2026@users.noreply.github.com>
2021-08-29 09:26:10 +0200

    Update calculator.rs

--- calculator.rs
+++ calculator.rs
@@ -1,10 +1,6 @@
+
 //needs to be renamed to main.rs
 use std::io;
-use rand::Rng;
-
-
-
-
 fn input(user_message: &str) -> io::Result<String> {
     use std::io::Write;
 
@@ -18,40 +14,31 @@ fn input(user_message: &str) -> io::Result<String> {
 
     Ok(buffer.trim_end().to_owned())
 }
-
 fn main () {
-    let mut rng = rand::thread_rng();
-    const MAXWRONG: u8 = 6;
-    println!("Welcome to the guess the number game!!");
-    let number: u8 = rng.gen_range(1..101);
-    let mut tries: u8 = 1;
-    let mut guess: u8 = input("Input number between 1-100: ")
+    let num1 = input("Input number: ")
         .unwrap()
-        .parse::<u8>()
+        .parse::<i32>()
+        .expect("Expected a number");
+    let operator = input("Input operator +, -, /, *: ").unwrap();
+    let num2 = input("Input number: ")
+        .unwrap()
+        .parse::<i32>()
         .expect("Expected a number");
 
-    while guess != number {
-        //println!("{}", number);
-        if guess > number {
-            println!("Lower....");
-        }
-        else if guess < number {
-            println!("Higher....")
-        } else {
-            println!("Correct");
-            break;
-        }
-        tries += 1;
-        guess = input("Input number between 1-100: ")
-            .unwrap()
-            .parse::<u8>()
-            .expect("Expected a number");
+    if operator == "+" {
+        let total: i32 = num1 + num2;
+        println!("{} {} {} = {}", num1, operator, num2, total);
     }
-    if tries <= MAXWRONG {
-        println!("You win!");
-        println!("You had {} guesses and the max is {}", tries, MAXWRONG)
-    } else {
-        println!("To many guesses!!");
-        println!("You had {} guesses but the max is {}", tries, MAXWRONG)
+    if operator == "-" {
+        let total: i32 = num1 - num2;
+        println!("{} {} {} = {}", num1, operator, num2, total);
+    }
+    if operator == "/" {
+        let total: i32 = num1 / num2;
+        println!("{} {} {} = {}", num1, operator, num2, total);
+    }
+    if operator == "*" {
+        let total: i32 = num1 * num2;
+        println!("{} {} {} = {}", num1, operator, num2, total);
     }
 }
